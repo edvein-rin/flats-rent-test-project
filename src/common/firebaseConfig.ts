2f6b463bcba52d3cd05935a98b4be496da328a51
edvein-rin <edveinrin@gmail.com>
2022-05-06 13:58:03 +0300

    Firebase configuration

--- src/common/firebaseConfig.ts
+++ src/common/firebaseConfig.ts
@@ -1,12 +1,12 @@
 // eslint-disable-next-line import/no-mutable-exports, @typescript-eslint/ban-types
 const firebaseConfig: Object = {
-  apiKey: 'xxx',
-  authDomain: 'xxx',
-  projectId: 'xxx',
-  storageBucket: 'xxx',
-  messagingSenderId: 'xxx',
-  appId: 'xxx',
-  measurementId: 'xxx',
+  apiKey: 'AIzaSyBjQWpu78y0VtwLsHQ-BNlO-kcGonKITv8',
+  authDomain: 'voypost-tp-eduard-tsakhlo.firebaseapp.com',
+  projectId: 'voypost-tp-eduard-tsakhlo',
+  storageBucket: 'voypost-tp-eduard-tsakhlo.appspot.com',
+  messagingSenderId: '99735181591',
+  appId: '1:99735181591:web:969a67978fd916df21ff52',
+  measurementId: 'G-JRBJPY2H1S',
 };
 
 export default firebaseConfig;
